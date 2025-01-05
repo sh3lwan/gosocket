7fd12f19b6be0de155800fe3cb251218b05d2250
Shaban Olwan <sh.3ban@live.com>
2025-01-05 11:33:06 +0200

    replace app port with .env

--- cmd/chatapp/main.go
+++ cmd/chatapp/main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	. "github.com/sh3lwan/gosocket/internal"
 	. "github.com/sh3lwan/gosocket/internal/server"
-	"log"
-)
-
-const (
-	PORT = "8080"
 )
 
 func main() {
-	srv := NewServer(PORT)
+    err := godotenv.Load()
+
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    port := os.Getenv("APP_PORT")
+
+	srv := NewServer(port)
 
 	HandleRoutes()
 
-	err := srv.Start()
+	err = srv.Start()
 
 	log.Fatal(err.Error())
 }
